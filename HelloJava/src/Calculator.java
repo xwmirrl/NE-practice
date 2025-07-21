5d2e1064880fc0f60bd85d24da8e95f314e9d5e4
xwmirrl <kayermir@gmail.com>
2025-07-21 17:22:49 +0500

    Calculator chotam
    # Conflicts:
    #	HelloJava/HelloJava.iml
    #	HelloJava/src/RoundDivide.java
    #	HelloJava/src/WholeFraction.java

---
+++ HelloJava/src/Calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String[] command = in.nextLine().split(" ");
+        double operand1 = Double.parseDouble(command[0]);
+        double operand2 = Double.parseDouble(command[2]);
+        String operation = command[1];
+        double result   ;
+
+        switch (operation) {
+
+            case "+":
+                result = operand1 + operand2;
+                break;
+
+            case "-":
+                result = operand1 - operand2;
+                break;
+
+            case "*":
+                result = operand1 * operand2;
+                break;
+
+            case "/":
+                result = operand1 / operand2;
+                break;
+
+            default:
+                System.out.println("Некорректная операция");
+                return;
+
+        }
+
+        System.out.printf("%.2f %s %.2f = %.2f\n", operand1, operation, operand2, result);
+
+
+    }
+
+}
